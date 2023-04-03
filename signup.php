1c16ad2a18565f9ef4070a1eb3135b917fee1648
Areitsu <luievtreyes@gmail.com>
2023-04-03 15:58:10 -0700

    updating login/signup

---
+++ signup.php
@@ -0,0 +1,33 @@
+<?php
+// Connect to the MySQL database
+$servername = "mysql://root:urnWYEZcoaCBS0LFv1et@containers-us-west-202.railway.app:6183/railway";
+$username = "root";
+$password = "urnWYEZcoaCBS0LFv1et";
+$dbname = "railway";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Retrieve user input from the form
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+// Hash the password before storing it in the database
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+// Execute an SQL query to insert the user data into the database
+$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+
+if ($conn->query($sql) === TRUE) {
+    // Redirect the user to a success page
+    header("Location: success.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn
+?>
