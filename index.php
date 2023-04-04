5e8546300a34d52b4cabc000caf13fb85c646f5e
Areitsu <luievtreyes@gmail.com>
2023-04-03 17:52:18 -0700

    a

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>GoVendor</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    
+    <script>
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- //Meta tag Keywords -->
+    <!-- Custom-Files -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <!-- Style-CSS -->
+    <!-- font-awesome-icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome-icons -->
+    <!-- /Fonts -->
+    <link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet">
+    <!-- //Fonts -->
+
+</head>
+
+<body>
+    <div class="main-sec">
+        <!-- //header -->
+        <header class="py-sm-3 pt-3 pb-2" id="home">
+            <div class="container">
+                <!-- nav -->
+                <div class="top-w3pvt d-flex">
+                    <div id="logo">
+                        <h1> <a href="index.html"><span class="log-w3pvt">G</span>oVendor</a> 
+                    </div>
+
+                    <div class="forms ml-auto">
+                        <a href="login.html" class="btn"><span class="fa fa-user-circle-o"></span> Sign In</a>
+                        <a href="register.html" class="btn"><span class="fa fa-pencil-square-o"></span> Sign Up</a>
+                    </div>
+                </div>
+                <div class="nav-top-wthree">
+                    <nav>
+                        <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
+                        <input type="checkbox" id="drop" />
+                        <ul class="menu">
+                            <li class="active"><a href="index.html">Home</a></li>
+                            <li><a href="about.html">About Us</a></li>
+                            <li><a href="shop.html">Local Shops</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+
+                        </ul>
+                    </nav>
+                    <!-- //nav -->
+                    <div class="search-form ml-auto">
+                        <div class="form-w3layouts-grid">
+                            <form action="#" method="post" class="newsletter">
+                                <input class="search" type="search" placeholder="Search here..." required="">
+                                <button class="form-control btn" value=""><span class="fa fa-search"></span></button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </header>
+        <!-- //header -->
+        <!--/banner-->
+        <div class="banner-wthree-info container">
+            <div class="row">
+                <div class="col-lg-5 banner-left-info">
+                    <h3>Find Vendors <span>Near You</span></h3>
+                    <a href="shop.html" class="btn shop">Search</a>
+                </div>
+
+                <div class="col-lg-7 banner-img">
+                    <img src="images/intro_03-2000.jpg"   alt="part image" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- //banner-->
+    <!--/banner-bottom -->
+    <section class="banner-bottom py-5">
+        <div class="container py-md-3">
+            <div class="row grids-wthree-info text-center">
+                <div class="col-lg-4 ab-content">
+                    <div class="ab-info-con">
+                        <h4>Sign up and become a Vendor</h4>
+                        <p>When you sign up with GoVendor. You have access to posting your goods. </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 ab-content">
+                    <div class="ab-info-con">
+                        <h4>Search for goods</h4>
+                        <p>Everyone who signs up with GoVendor has access to find goods they are looking for with our simple search menu.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 ab-content">
+                    <div class="ab-info-con">
+                        <h4>Continue</h4>
+                        <p>Continue</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+ 
+    <!-- /banner-bottom -->
+    <!--/collections -->
+    <section class="banner-bottom py-5">
+        <div class="container py-md-5">
+
+            <h3 class="title-wthree mb-lg-5 mb-4 text-center"> Meet local Vendors </h3>
+            <div class="row text-center">
+
+
+                <div class="col-md-4 content-gd-wthree">
+                    <img src="images/Vendor2.jpg" class="img-fluid" alt="" />
+                </div>
+                <div class="col-md-4 content-gd-wthree ab-content py-lg-5 my-lg-5">
+                    <h4>Want to search?</h4>
+                    <p>Supporting our local vendors is what we are all about.</p>
+                    <a href="shop.html" class="btn shop mt-3">Search Vendors</a>
+
+                </div>
+                <div class="col-md-4 content-gd-wthree">
+                    <img src="images/vendor3.jpg" class="img-fluid" alt="" />
+                </div>
+            </div>
+
+        </div>
+    </section>
+  
+    <section class="shipping-wthree">
+        <div class="shiopping-grids d-lg-flex">
+            <div class="col-lg-4 shiopping-w3pvt-gd text-center">
+                <div class="icon-gd"><span class="fa fa-truck" aria-hidden="true"></span>
+                </div>
+                <div class="icon-gd-info">
+                    <h3>Start up shop anywhere</h3>
+                </div>
+            </div>
+            <div class="col-lg-4 shiopping-w3pvt-gd sec text-center">
+                <div class="icon-gd"><span class="fa fa-bullhorn" aria-hidden="true"></span></div>
+                <div class="icon-gd-info">
+                    <h3>Announce your location on GoVendor</h3>
+                </div>
+            </div>
+            <div class="col-lg-4 shiopping-w3pvt-gd text-center">
+                <div class="icon-gd"> <span class="fa fa-gift" aria-hidden="true"></span></div>
+                <div class="icon-gd-info">
+                    <h3>Join Today</h3>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+    <!--//shipping-->
+    <!-- footer -->
+    <div class="footer_agileinfo_topf py-5">
+        <div class="container py-md-5">
+            <div class="row">
+                <div class="col-lg-3 footer_wthree_gridf mt-lg-5">
+                    <h2><a href="index.html"><span>G</span>oVendor
+                        </a> </h2>
+                </div>
+                <div class="col-lg-3 footer_wthree_gridf mt-md-0 mt-4">
+                    <ul class="footer_wthree_gridf_list">
+                        <li>
+                            <a href="index.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Home</a>
+                        </li>
+                        <li>
+                            <a href="about.html"><span class="fa fa-angle-right" aria-hidden="true"></span> About</a>
+                        </li>
+                     
+                        <li>
+                          
+                    </ul>
+                </div>
+                <div class="col-lg-3 footer_wthree_gridf mt-md-0 mt-sm-4 mt-3">
+                    <ul class="footer_wthree_gridf_list">
+                       
+
+                        <li>
+                            <a href="#"><span class="fa fa-angle-right" aria-hidden="true"></span> Terms & Conditions</a>
+                        </li>
+                  
+                        <li>
+                            <a href="contact.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 footer_wthree_gridf mt-md-0 mt-sm-4 mt-3">
+                    <ul class="footer_wthree_gridf_list">
+                        <li>
+                            <a href="login.html"><span class="fa fa-angle-right" aria-hidden="true"></span> Login </a>
+                        </li>
+
+                        <li>
+                            <a href="register.html"><span class="fa fa-angle-right" aria-hidden="true"></span>Register</a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="fa fa-angle-right" aria-hidden="true"></span>Privacy & Policy</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+
+            <div class="w3ls-fsocial-grid">
+                <h3 class="sub-w3ls-headf">Follow Us</h3>
+                <div class="social-ficons">
+                    <ul>
+                        <li><a href="#"><span class="fa fa-facebook"></span> Facebook</a></li>
+                        <li><a href="#"><span class="fa fa-twitter"></span> Twitter</a></li>
+                        <li><a href="#"><span class="fa fa-google"></span>Google</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="move-top text-center mt-lg-4 mt-3">
+                <a href="#home"><span class="fa fa-angle-double-up" aria-hidden="true"></span></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="cpy-right text-center py-3">
+        <p>© 2022 GoVendor. All rights reserved 
+        </p>
+
+    </div>
+    <!-- //copyright -->
+
+</body>
+
+</html>
